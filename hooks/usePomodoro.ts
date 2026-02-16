ee676a71bc0aea96581e063d61c47f6b900319e3
Rui <214510304+Rui-moment@users.noreply.github.com>
2026-02-16 16:40:49 +0900

    Initial commit: ポモドーロ基本機能完成

---
+++ hooks/usePomodoro.ts
@@ -0,0 +1,11 @@
+"use client";
+import { useState, useEffect } from "react";
+
+type phase = "work" | "rest";
+
+const WORK_TIME = 10;
+const REST_TIME = 5;
+
+export function usePomodoro() {
+    //ここに実装していく
+}
